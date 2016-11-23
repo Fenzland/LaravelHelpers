5804ffc37aafb3d32561a225ef9b34761d7b78b7
Fenz <uukooo@126.com>
2016-11-23 11:08:37 +0800

    Add "strrstr" php helper

--- libs/php-helpers.php
+++ libs/php-helpers.php
@@ -17,3 +17,20 @@ if( !function_exists('array_transposition') )
 		return array_combine( array_keys( $values[0] ), array_map( function( ...$args )use( $keys ){  return array_combine( $keys, $args );  }, ...$values ) );
 	}
 }
+
+
+if( !function_exists('strrstr') ){
+	/**
+	 * strstr from right to left.
+	 *
+	 * @param  string $haystack
+	 * @param  mixed  $needle
+	 * @param  boll   $before_needle
+	 *
+	 * @return string
+	 */
+	function strrstr( string$haystack , $needle, bool$before_needle=false )
+	{
+		return strrev(strstr( strrev($haystack), $needle, $before_needle ));
+	}
+}
