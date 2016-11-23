b541d8fb521f1f080cb4a96bea41f0f5c165c1a1
Fenz <uukooo@126.com>
2016-11-23 11:53:58 +0800

    Split file to two files by within or without libraries dependence

--- libs/laravel-helpers.php
+++ libs/laravel-helpers.php
@@ -52,19 +52,3 @@ if (! function_exists('routo')) {
 		return app('url')->route($name,$parameters,$absolute);
 	}
 }
-
-
-if (! function_exists('array_transposition')) {
-	/**
-	 * Transposition an 2-dimensional array.
-	 *
-	 * @param  array
-	 * @return array
-	 */
-	function array_transposition( array$input )
-	{
-		$keys=   array_keys($input);
-		$values= array_values($input);
-		return array_combine(array_keys($values[0]),array_map(function(...$args)use($keys){return array_combine($keys,$args);},...$values));
-	}
-}
---
+++ libs/php-helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if (! function_exists('array_transposition')) {
+	/**
+	 * Transposition an 2-dimensional array.
+	 *
+	 * @param  array
+	 * @return array
+	 */
+	function array_transposition( array$input )
+	{
+		$keys=   array_keys($input);
+		$values= array_values($input);
+		return array_combine(array_keys($values[0]),array_map(function(...$args)use($keys){return array_combine($keys,$args);},...$values));
+	}
+}
